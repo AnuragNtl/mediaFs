ea5f1b519acfe325c42794565cb049ed3056ce8d
AnuragNtl <anuragshrm70@gmail.com>
2023-04-15 16:47:00 +0530

    dir handling

--- src/fuseWrapper/FuseWrapper.cpp
+++ src/fuseWrapper/FuseWrapper.cpp
@@ -1,14 +1,18 @@
 #include <cstring>
 #include <unistd.h>
 #include <iostream>
+#include <functional>
 
 #include "FuseWrapper.h"
 
 
-int readDir(const char *path, void *buffer, fuse_fill_dir_t fillter,
+int readDir(const char *path, void *buffer, fuse_fill_dir_t filler,
         off_t offset,
         struct fuse_file_info *fi) {
     std::cout << "Path readDir " << path << "\n";
+    filler(buffer, ".", NULL, 0);
+    filler(buffer, "..", NULL, 0);
+    filler(buffer, "s2", NULL, 0);
     return 0;
 }
 
@@ -19,6 +23,10 @@ int read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_
     return 10;
 }
 
+int open(const char *path, struct fuse_file_info *fi) {
+    return 0;
+}
+
 int getAttr(const char *path, struct stat *st) {
 
     std::cout << "Path getAttr " << path << "\n";
@@ -27,10 +35,16 @@ int getAttr(const char *path, struct stat *st) {
     st->st_atime = time(NULL);
     st->st_mtime = time(NULL);
     st->st_nlink = 1;
-    st->st_mode = S_IFREG | 0644;
-    st->st_size = 20;
+    if (std::string(path) == "/") {
+        st->st_mode = S_IFDIR | 0755;
+        st->st_nlink = 2;
+    } else {
+        st->st_mode = S_IFREG | 0644;
+        st->st_size = 20;
+    }
     return 0;
 }
+
 
 int ioctlHandler(const char *, int, void *, struct fuse_file_info *info, unsigned int, void*) {
     std::cout << "ioctl \n";
