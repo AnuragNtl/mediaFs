ce56a7a2fbd47ac5436db423f354477e18b3a293
Anurag <anurag>
2024-01-27 08:21:32 +0530

    _______

--- src/fsprovider/server/Server.cpp
+++ src/fsprovider/server/Server.cpp
@@ -5,148 +5,27 @@
 
 namespace MediaFs {
 
-    template<class I, class T>
-    Node<I, T> :: Node(I index, T &value, Node<I, T> *previous) : index(index), value(value), previous(previous) {
-        if (previous != NULL) {
-            previous->next = this;
-        }
-        next = NULL;
-    }
-
-    template<class I, class T>
-    Node<I, T>* Node<I, T> :: getNext() {
-        return next;
-    }
-
-    template<class I, class T>
-    Node<I, T>* Node<I, T> :: getPrevious() {
-        return previous;
-    }
-
-    template<class I, class T>
-    void Node<I, T> :: append(Node<I, T> *previous) {
-        this->previous = previous;
-        previous->next = this;
-        this->next = NULL;
-    }
-
-    template<class I, class T>
-    void Node<I, T> :: detach() {
-        if (this->next != NULL) {
-            this->next->previous = this->previous;
-        }
-        if (this->previous != NULL) {
-            this->previous->next = this->next;
-        }
-        this->next = NULL;
-        this->previous = NULL;
-    }
-
-    template<class I, class T>
-    LRUCache<I, T> :: LRUCache(int length) : length(length), _onFull([] (I, T) { }), currentLength(0)  { }
-
-    template<class I, class T>
-    T& LRUCache<I, T> :: operator[](I index) {
-        if (location.find(index) == location.end()) {
-            throw InvalidItemException("index not found");
-        }
-        globalMutex.lock();
-        auto node = location[index];
-        node->detach();
-        node->append(tail);
-        globalMutex.unlock();
-        return node->value;
-    }
-
-    template<class I, class T>
-    void LRUCache<I, T> :: add(I index, T &item) {
-        if (location.find(item) != location.end()) {
-            throw InvalidItemException("item already present");
-        }
-        globalMutex.lock();
-        Node<I, T> *node;
-        if (currentLength >= length) {
-            T &headValue = head->value;
-            evict(head->index);
-            _onFull(index, headValue);
-        }
-        if (head == NULL) {
-            node = new Node<I, T>(item);
-            head = node;
-            tail = node;
-        } else {
-            node = new Node<I, T>(item, tail);
-            tail = node;
-        }
-        currentLength++;
-        globalMutex.unlock();
-    }
-
-    template<class I, class T>
-    void LRUCache<I, T> :: evict(I index) {
-        if (location.find(index) == location.end()) {
-            throw new InvalidItemException("Item not found");
-        }
-        Node<I, T> *node = location[index];
-        location.erase(index);
-        evictNode(node);
-        currentLength--;
-    }
-
-    template<class I, class T>
-    void LRUCache<I, T> :: evictNode(Node<I, T> *node) {
-        if (node == head) {
-            head = head->next;
-        } else if (node == tail) {
-            tail = tail->previous;
-        }
-        node->detach();
-        delete node;
-    }
-
-    template<class I, class T>
-    void LRUCache<I, T> :: onFull(std::function<void(I, const T&)> _onFull) {
-        this->_onFull = _onFull;
-    }
-
-    template<class I, class T>
-    LRUCache<I, T> :: ~LRUCache() {
-        globalMutex.lock();
-        if (head == NULL) {
-            return;
-        }
-        for (auto node = head->next; node != NULL; node = node->next) {
-            delete node->previous;
-        }
-        delete tail;
-        globalMutex.unlock();
-    }
-
     InvalidItemException :: InvalidItemException(std::string message) : message(message) { }
 
-    Server :: Server(int openHandlesSize) : openHandlesSize(openHandlesSize) { }
-
-
-    int Server :: read(std::string path, char *buffer, int size, int offset) const {
-        FileHandle *handle = openHandles->find(path);
-        std::ifstream in(path, std::ios::in | std::ios::binary);
-        char *buf = new char[size];
-        in.seekg(offset, std::ios::beg);
-        in.read(buf, size);
-        in.close();
-        //TODO
-        return 0;
+    Server :: Server(int openHandlesSize) : openHandlesSize(openHandlesSize), openHandles(LRUCache<std::string, FileCache*>(openHandlesSize)) {
     }
 
-    Server :: ~Server() {
-        for (auto pair : openHandles) {
-            delete[] pair.second;
+    int Server :: read(std::string path, char *buffer, int offset, int size) {
+        if (!openHandles.has(path)) {
+            openHandles.add(path, new FileCache(std::unique_ptr<std::ifstream>(new std::ifstream(path, std::ios::in | std::ios::binary))));
         }
+        FileCache &handle = *openHandles[path];
+        auto data = handle[std::make_pair(offset, offset + size)];
+        memcpy(buffer, std::get<0>(data), size);
+        return std::get<1>(data);
     }
+
+    Server :: ~Server() { }
 
     FileCache :: FileCache(std::unique_ptr<std::ifstream> &&fileHandle) : fileHandle(std::move(fileHandle)) { }
 
     void FileCache :: refreshRanges() {
+        handleMutex.lock();
         std::list<std::pair<int, std::unique_ptr<Buffer> > > ranges;
         for (auto buffer = buffers.begin(); buffer != buffers.end(); buffer++) {
             std::pair<int, std::unique_ptr<Buffer> > range(buffer->first, std::move(buffer->second));
@@ -189,14 +68,31 @@ namespace MediaFs {
             buffers[item.first] = std::move(item.second);
         }
 
+        handleMutex.unlock();
+    }
 
+    std::tuple<const char*, int> FileCache :: operator[](std::pair<int, int> range) {
+        handleMutex.lock();
+        for (auto &buffer : buffers) {
+            if ((range.first >= buffer.first) && (range.second <= (buffer.first + buffer.second->size))) {
+                handleMutex.unlock();
+                return {buffer.second->data + (range.first - buffer.first), range.second - range.first};
+            }
+        }
+        fileHandle->seekg(range.first);
+        char *data = new char[range.second];
+        fileHandle->read(data, range.second);
+        buffers[range.first] = std::make_unique<Buffer>();
+        buffers[range.first]->data = data;
+        buffers[range.first]->size = fileHandle->gcount();
+        handleMutex.unlock();
+        std::thread rangeRefresher(FileCache::refreshRangesInBackground, this);
+        rangeRefresher.detach();
+        return {buffers[range.first]->data, buffers[range.first]->size};
+    }
 
-
-        /*for (auto range : ranges) {
-
-          }*/
-
-
+    void FileCache :: refreshRangesInBackground(FileCache *fileCache) {
+        fileCache->refreshRanges();
     }
 
     std::pair<int, std::unique_ptr<Buffer>>* combineRanges(std::pair<int, std::unique_ptr<Buffer> > &firstRange, std::pair<int, std::unique_ptr<Buffer> > &secondRange) {
@@ -230,5 +126,5 @@ namespace MediaFs {
     Attr Server :: getAttr(std::string path) const {
         return Attr{};
     }
-
 }
+
--- src/fsprovider/server/Server.h
+++ src/fsprovider/server/Server.h
@@ -2,14 +2,18 @@
 
 #define SERVER_H
 
-#include "../FSProvider.h"
 #include <iostream>
 #include <map>
 #include <functional>
 #include <memory>
 #include <mutex>
+#include <tuple>
+#include <thread>
+
+#include "../FSProvider.h"
 
 #define DEFAULT_OPEN_HANDLES_SIZE 10
+
 
 namespace MediaFs {
 
@@ -18,26 +22,16 @@ namespace MediaFs {
         private:
             Node<I, T> *next;
             Node<I, T> *previous;
-            T &value;
+            T value;
             I index;
         public:
-            Node(I index, T &value, Node<I, T> *previous=NULL);
+            Node(I index, T value, Node<I, T> *previous=NULL);
             Node<I, T>* getNext();
             Node<I, T>* getPrevious();
             void append(Node<I, T> *);
             void detach();
             virtual ~Node() = default;
             template<class I1, class T1> friend class LRUCache;
-    };
-
-    class InvalidItemException : public std::exception {
-        private:
-            std::string message;
-        public:
-            InvalidItemException(std::string message);
-            const char* what() const noexcept {
-                return message.c_str();
-            }
     };
 
     template<class I, class T>
@@ -56,9 +50,148 @@ namespace MediaFs {
             bool has(I);
             T& operator[](I);
             void onFull(std::function<void(I, const T&)>);
-            void add(I, T&);
+            void add(I, T);
             virtual ~LRUCache();
     };
+
+    class InvalidItemException : public std::exception {
+        private:
+            std::string message;
+        public:
+            InvalidItemException(std::string message);
+            const char* what() const noexcept {
+                return message.c_str();
+            }
+    };
+
+    template<class I, class T>
+    Node<I, T> :: Node(I index, T value, Node<I, T> *previous) : index(index), value(value), previous(previous) {
+        if (previous != NULL) {
+            previous->next = this;
+        }
+        next = NULL;
+    }
+
+    template<class I, class T>
+    Node<I, T>* Node<I, T> :: getNext() {
+        return next;
+    }
+
+    template<class I, class T>
+    Node<I, T>* Node<I, T> :: getPrevious() {
+        return previous;
+    }
+
+    template<class I, class T>
+    void Node<I, T> :: append(Node<I, T> *previous) {
+        this->previous = previous;
+        previous->next = this;
+        this->next = NULL;
+    }
+
+    template<class I, class T>
+    void Node<I, T> :: detach() {
+        if (this->next != NULL) {
+            this->next->previous = this->previous;
+        }
+        if (this->previous != NULL) {
+            this->previous->next = this->next;
+        }
+        this->next = NULL;
+        this->previous = NULL;
+    }
+
+    template<class I, class T>
+    LRUCache<I, T> :: LRUCache(int length) : length(length), _onFull([] (I, T) { }), currentLength(0), head(NULL), tail(NULL)  { }
+
+    template<class I, class T>
+    T& LRUCache<I, T> :: operator[](I index) {
+        if (location.find(index) == location.end()) {
+            throw InvalidItemException("index not found");
+        }
+        globalMutex.lock();
+        auto node = location[index];
+        if (node == head) {
+            head = node->next;
+        }
+        node->detach();
+        node->append(tail);
+        tail = node;
+        globalMutex.unlock();
+        return node->value;
+    }
+
+    template<class I, class T>
+    void LRUCache<I, T> :: add(I index, T item) {
+        if (location.find(index) != location.end()) {
+            throw InvalidItemException("item already present");
+        }
+        globalMutex.lock();
+        Node<I, T> *node;
+        if (currentLength >= length) {
+            T &headValue = head->value;
+            evict(head->index);
+            _onFull(index, headValue);
+        }
+        if (head == NULL) {
+            node = new Node<I, T>(index, item);
+            head = node;
+            tail = node;
+        } else {
+            node = new Node<I, T>(index, item, tail);
+            tail = node;
+        }
+        location[index] = node;
+        currentLength++;
+        globalMutex.unlock();
+    }
+
+    template<class I, class T>
+    bool LRUCache<I, T> :: has(I index) {
+        return location.find(index) != location.end();
+    }
+
+    template<class I, class T>
+    void LRUCache<I, T> :: evict(I index) {
+        if (location.find(index) == location.end()) {
+            throw new InvalidItemException("Item not found");
+        }
+        Node<I, T> *node = location[index];
+        location.erase(index);
+        evictNode(node);
+        currentLength--;
+    }
+
+    template<class I, class T>
+    void LRUCache<I, T> :: evictNode(Node<I, T> *node) {
+        if (node == head) {
+            head = head->next;
+        } else if (node == tail) {
+            tail = tail->previous;
+        }
+        node->detach();
+        delete node;
+    }
+
+    template<class I, class T>
+    void LRUCache<I, T> :: onFull(std::function<void(I, const T&)> _onFull) {
+        this->_onFull = _onFull;
+    }
+
+    template<class I, class T>
+    LRUCache<I, T> :: ~LRUCache() {
+        globalMutex.lock();
+        if (head == NULL) {
+            globalMutex.unlock();
+            return;
+        }
+        for (auto node = head->next; node != NULL; node = node->next) {
+            delete node->previous;
+        }
+        delete tail;
+        globalMutex.unlock();
+    }
+
 
     struct Buffer {
         char *data;
@@ -75,10 +208,15 @@ namespace MediaFs {
     };
 
     struct FileCache {
-        std::unique_ptr<std::ifstream> &&fileHandle;
-        FileCache(std::unique_ptr<std::ifstream> &&);
-        std::map<int, std::unique_ptr<Buffer> > buffers;
-        void refreshRanges();
+        private:
+            static void refreshRangesInBackground(FileCache *fileCache);
+        public:
+            std::mutex handleMutex;
+            std::map<int, std::unique_ptr<Buffer> > buffers;
+            std::unique_ptr<std::ifstream> &&fileHandle;
+            FileCache(std::unique_ptr<std::ifstream> &&);
+            void refreshRanges();
+            std::tuple<const char*, int> operator[](std::pair<int, int>);
     };
 
     class Server : public FSProvider {
@@ -88,11 +226,21 @@ namespace MediaFs {
         public:
             Server(int openHandlesSize = DEFAULT_OPEN_HANDLES_SIZE);
             Server(const Server &) = delete;
-            int read(std::string path, char *buffer, int size, int offset) const;
+            int read(std::string path, char *buffer, int, int);
             std::vector<Attr> readDir(std::string path) const;
             Attr getAttr(std::string path) const;
             virtual ~Server();
 
+    };
+
+    class FileCacheException : std::exception {
+        private:
+            std::string message;
+        public:
+            FileCacheException(std::string message) : message(message) { }
+        const char* what() noexcept {
+            return message.c_str();
+        }
     };
 
     extern std::pair<int, std::unique_ptr<Buffer> >* combineRanges(std::pair<int, std::unique_ptr<Buffer> > &, std::pair<int, std::unique_ptr<Buffer> > &);
