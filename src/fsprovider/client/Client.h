18668ccb8a99a55409fd5fa49de7d56d95a410e9
Anurag <anurag>
2024-05-17 14:30:37 +0530

    ______

--- src/fsprovider/client/Client.h
+++ src/fsprovider/client/Client.h
@@ -13,13 +13,15 @@ namespace MediaFs {
     class Client  : public FSProvider {
         private:
             ios io;
+            tcp::socket socket;
+            tcp::endpoint endpoint;
             // TODO FileCache should also process string keys 
             // which will be filenames, rather than file handles.
             // File names because the client would only know about them.
             LRUCache<std::string, FileCache<std::string>*> openHandles;
-            void sendRequest(std::vector<std::string>);
+            void sendRequest(std::string);
         public:
-            Client();
+            Client(int length = 10);
             const char* read(std::string path, int &size, int offset);
             std::vector<Attr> read(std::string path) const;
             Attr getAttr(std::string path) const;
