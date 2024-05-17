67529d40395a47e78983e549f95ad7db25d960d3
AnuragNtl <anuragshrm70@gmail.com>
2024-05-17 14:30:37 +0530

    ______

--- src/fsprovider/client/Client.cpp
+++ src/fsprovider/client/Client.cpp
@@ -1,14 +1,26 @@
 #include "./Client.h"
-#include "../../transfer/server/MetadataServer.h""
+#include "../../transfer/server/MetadataServer.h"
 
 namespace MediaFs {
 
-    Client :: Client() {
+    Client :: Client(int length) : openHandles(LRUCache<std::string, FileCache<std::string>*>(length)) {
+        socket = tcp::socket(io);
+        endpoint = tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8080);
+        socket.connect(endpoint);
     }
 
-    void Client :: sendRequest(std::vector<std::string>) {
-        tcp::socket s(io);
+    void Client :: sendRequest(std::string payload) {
         // TODO
+        if (!socket.is_open()) {
+            socket = tcp::socket(io);
+            socket.connect(endpoint);
+        }
+        boost::system::system_error error;
+        boost::asio::write(socket, boost::asio::buffer(payload), error);
+
+        if (error) {
+            throw std::exception();
+        }
     }
 
     const char* FSProvider :: read(std::string path, int &size, int offset) {
